64d5b2605bf2de70ca1feeabc7e1393ff2e3c882
Paul Thompson <mailto.prt@gmail.com>
2019-08-17 14:00:02 -0400

    add bundle process

---
+++ src/app.js
@@ -0,0 +1,6 @@
+// front end code
+const greeting = document.createElement("div");
+const body = document.querySelector('body');
+
+greeting.textContent = "Hello World";
+body.appendChild(greeting);
